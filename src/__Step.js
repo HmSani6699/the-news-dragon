10e8ef44c93a5c1d3fc876f76e33dc44ddf757f0
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-03 15:29:07 +0600

    added to marquee text

--- src/__Step.js
+++ src/__Step.js
@@ -1,6 +1,6 @@
 /**
- * 
- * 
+ * 1. [visit] to Moment.js == time setup er jonno use hoy
+ * 2. [visit] to Marquee 
  * 
  * 
  * 
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import logo from '../../../assets/logo.png';
+import moment from 'moment';
+import { Button, Container } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
+
 
 const Header = () => {
     return (
-        <div>
-            <h1>
-                this is header
-            </h1>
-        </div>
+        <Container>
+            <div className='text-center'>
+                <img src={logo} alt="" />
+                <p><small>Journalism Without Fear or Favour</small></p>
+                <p>{moment().format("dddd, MMMM D, YYYY")}</p>
+            </div>
+            <div className='d-flex'>
+                <Button variant="danger">Danger</Button>
+                <Marquee>
+                    I can be a React component, multiple React components, or just some text.
+                </Marquee>
+            </div>
+        </Container>
     );
 };
 
