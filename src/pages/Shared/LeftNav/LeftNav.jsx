a478c226905b763c36577b0b5cc85a26311eea32
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-03 23:25:36 +0600

    fetch to categoris part

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LeftNav = () => {
+
+    const [categoris, setCategoris] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categoris')
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
+    }, [])
     return (
         <div>
             <h2>left nav</h2>
