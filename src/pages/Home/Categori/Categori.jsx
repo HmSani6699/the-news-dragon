85c0b64a66fec612f6755380e8b67e06b3a4b6b6
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-04 08:19:15 +0600

    ade to useprams hook

---
+++ src/pages/Home/Categori/Categori.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const Categori = () => {
+
+    const {id }=useParams();
+
+    return (
+        <div>
+            <h1>this is category{id}</h1>
+        </div>
+    );
+};
+
+export default Categori;
--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
 
@@ -7,12 +8,19 @@ const LeftNav = () => {
     useEffect(() => {
         fetch('http://localhost:5000/categoris')
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setCategoris(data))
             .catch(error => console.log(error))
     }, [])
     return (
         <div>
-            <h2>left nav</h2>
+            <h2>AllGategoris</h2>
+
+               {
+                categoris.map(categori=><p
+                key={categori.id}>
+                    <Link className='text-dacoretion-none' to={`/categor/${categori.id}`}>{categori.name}</Link>
+                </p>)
+               }
         </div>
     );
 };
